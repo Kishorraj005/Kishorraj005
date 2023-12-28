bf5625ce043ff0ad396b20b40af5e65699801ee7
Kishorraj005 <147033414+Kishorraj005@users.noreply.github.com>
2023-12-28 09:54:08 +0530

    Add files via upload

---
+++ square.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class square 
+{
+    public static void main(String args[])
+    {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("enter the number:");
+        int a=obj.nextInt();
+        int b=(a*a);
+        System.out.println(+b);
+    }
+}
